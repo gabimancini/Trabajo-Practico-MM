d2f907eb89aa58e6061767953f15ac0ed139e246
marcosvillabasa <marcosvillabasa@gmail.com>
2018-11-07 10:47:09 -0300

    modelo Persona realizado, relacion con usuario

---
+++ BackServer/Modelos/persona.js
@@ -0,0 +1,14 @@
+let mongoose = require('mongoose');
+let Schema = mongoose.Schema;
+
+let personaSchema = new Schema ({
+  nombre: { type: String, required: [true, 'El nombre es necesario']},
+  apellido: { type: String, required: [true, 'El apellido es necesario']},
+  legajo: { type: String, unique: true, required: [true, 'El legajo es necesario']},
+  rol: {type: String, required: true},
+  img: {type: Strig, required: false},
+  usuario: {type: Schema.Types.ObjectId, ref:'Usuario', required: [true, 'El usuario es requerido']},
+
+});
+
+module.exports = mongoose.model('Persona', personaSchema);
