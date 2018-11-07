42a94ce7161522e290b36dd5da9d26241908e210
gabimancini <gabimancinir@gmail.com>
2018-11-07 12:06:32 -0300

    Importación de servicios y componentes servicios de empresa

--- Cliente/src/app/app.module.ts
+++ Cliente/src/app/app.module.ts
@@ -4,17 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
+import { ServiciosEmpresaComponent } from './pages/servicios-empresa/servicios-empresa.component';
+
+//Services
+import  { ServiciosEmpresaService  } from './services/servicios-empresa.service';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    ServiciosEmpresaComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [ServiciosEmpresaService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
