1770d3c97b0be11f2270c8b72eac54634d91098c
marcosvillabasa <marcosvillabasa@gmail.com>
2018-11-08 17:35:37 -0300

    Creamos metodo crearUsuarios

--- BackServer/Controladores/usuario.js
+++ BackServer/Controladores/usuario.js
@@ -1,6 +1,6 @@
 //importamos los modelos de la BD
 var  Usuario = require('../Modelos/usuario.js');
-
+var bcrypt = require('bcryptjs');
 
 
 
@@ -23,10 +23,71 @@ let obtenerUsuarios = (req, res) => {
         });
       }
 
-    })
+    });
 }
 
+//-------Crear Usuario--------
+let crearUsuario = (req, res) => {
+
+  var body = req.body;
+
+  // res.status(200).json({
+  //   ok:true,
+  //   body:body
+  // })
+  var usuarioNuevo = new Usuario(
+    {
+      nombre: body.nombre,
+      email: body.email,
+      password: body.password,
+      img: body.img,
+      rol: body.rol
+    }
+  ); // creamos el nuevo obj
+
+  usuarioNuevo.save( (err,usuarioGuardado) => {
+    if (err) {
+      return res.status(500).json({
+        ok: false,
+        mensaje: "Error al crear el user",
+        errors: err
+      });
+    }
+    res.status(200).json({
+      ok: true,
+      usuario: usuarioGuardado
+    })
+  });
+
+  // if (!body.nombre || body.email) {
+  //   return res.status(500).json({
+  //     ok: false,
+  //     mensaje: "Error al crear usuario, campos incompletos"
+  //   });
+  // }
+  // if (!body.password) {
+  //   return res.status(500).json({
+  //     ok: false,
+  //     mensaje: "Error al crear usuario"
+  //   })
+  // }
+
+  // usuarioNuevo.save( (err, usuarioGuardado) =>{
+  //   res.status(200).json({
+  //     ok: true,
+  //     usuario: usuarioGuardado
+  //   });
+  // });
+
+  // usuario.save().then((usuarioGuardado)=>{
+  //   return res.send({
+  //     usuario:usuarioGuardado,
+  //     // token:jwt.createToken(usuarioGuardado)
+  //   })
+  // })
+}
 
 module.exports = {
   obtenerUsuarios,
+  crearUsuario,
 }
