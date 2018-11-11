fd71d8a216ad0d87f00de845c1966b1ba4d34d79
marcosvillabasa <marcosvillabasa@gmail.com>
2018-11-11 15:11:12 -0300

    modificado el modelo ordenCarga, se agrego array de clientes al mismo

--- BackServer/Modelos/ordenCarga.js
+++ BackServer/Modelos/ordenCarga.js
@@ -3,13 +3,22 @@ let uniqueValidator = require('mongoose-unique-validator');
 
 let Schema = mongoose.Schema;
 
+let clienteSchema = new Schema ({
+  codigoCliente:{type:Number, required:true},
+  razonSocial: {type:String, required:true},
+  cuit: {type:Number, required:true},
+  descripcion:{type:String}
+})
+
 let ordenCargaSchema = new Schema({
-  codigo: {type: Number, required: [true, 'El codigo es necesario'},
+  codigo: {type: Number, required: [true, 'El codigo es necesario']},
   fecha: {type: Date, required: true},
   codLocalidadDesde: {type: Schema.Types.ObjectId, ref: 'Localidad' , required: true},
   codLocalidadHasta: {type: Schema.Types.ObjectId, ref: 'Localidad'}, required: true,
   camion: {type: Schema.Types.ObjectId, ref: 'Camion', required: true},
-
+  cliente: [clienteSchema]
 });
+
+;
 
 moduel.exports = mongoose.model ('ordenCarga', ordenCargaSchema);
