5c9de05c0afe92f273c8118ef170aecdcc58a6ae
marcosvillabasa <marcosvillabasa@gmail.com>
2018-11-12 21:11:54 -0300

    agregamos el atributo marca como un array embebido

--- BackServer/Modelos/camion.js
+++ BackServer/Modelos/camion.js
@@ -3,12 +3,17 @@ let uniqueValidator = require('mongoose-unique-validator');
 
 let Schema = mongoose.Schema;
 
+let marcaSchema = new Schema({
+  nombre: {type: String, required:true},
+  descripcion: {type: String}
+})
+
 let camionSchema = new Schema({
   patente: {type: String, unique:true, required: [true, 'La patente es necesaria']},
   capacidad: {type: Number, required:true},
   modelo: {type: String, required: false},
-  marca: {type: String},
   legajo:{ type: Schema.Types.ObjectId, ref: 'Persona', required: [true, 'El camion tiene que tener un responsable']},
+  marca: [marcaSchema]
 
 });
 
