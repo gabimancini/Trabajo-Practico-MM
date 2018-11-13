ebd68fb52b99fec1e5eb3d82e83cf5e7a1f1fcd0
marcosvillabasa <marcosvillabasa@gmail.com>
2018-11-13 03:46:46 -0300

    Agregadas las rutas para el CRUD del modelo camion.js

---
+++ BackServer/Rutas/camion.js
@@ -0,0 +1,20 @@
+'use strict'
+
+// importamos express
+const express = require('express');
+
+//importamos el controlador
+var CamionController = require('../Controladores/camion.js');
+
+//modulo Router de Express
+const app = express.Router()
+
+//Rutas para Camion
+app.get('/getCamiones', CamionController.obtenerCamiones);
+app.post('/registroCamion', CamionController.crearCamion);
+app.put('/:id', CamionController.actualizarCamion);
+app.delete('/:id', CamionController.borrarCamion);
+
+
+//exportamos el Router
+module.exports = app;
