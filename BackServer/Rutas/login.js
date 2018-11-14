e2da7737982071c145e34b871b7be973955a047a
marcosvillabasa <marcosvillabasa@gmail.com>
2018-11-14 04:46:52 -0300

    creamos el metodo para verificar email y pass, y crear el token asociado a ese usuario

---
+++ BackServer/Rutas/login.js
@@ -0,0 +1,14 @@
+'use strict'
+
+// importamos express
+const express = require('express');
+
+//importamos el controlador
+var LoginController = require('../Controladores/login.js');
+
+//modulo Router de Express
+const app = express.Router();
+
+app.post('/', LoginController.login);
+
+module.exports = app;
