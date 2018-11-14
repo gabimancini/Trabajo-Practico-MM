506473c33be42bccf0387a910eadc12022781032
marcosvillabasa <marcosvillabasa@gmail.com>
2018-11-14 04:40:21 -0300

    creamos el controlador login para verificar el login de un usuario con su email y pass

---
+++ BackServer/Controladores/login.js
@@ -0,0 +1,57 @@
+var express = require('express');
+var bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
+
+var app = express();
+
+var  Usuario = require('../Modelos/usuario.js');
+
+let login = (req, res) => {
+
+  var body = req.body;
+
+  Usuario.findOne({email: body.email} , (err, usuarioDB) => {
+
+    if (err) {
+      return res.status(500).json({
+        ok: false,
+        mensaje: "Error al buscar el email",
+        errors: err
+      });
+    }
+
+    if (!usuarioDB) {
+      return res.status(500).json({
+        ok: false,
+        mensaje: "Credenciales incorrectas - email",
+        errors: err
+      });
+    }
+
+    // if (!bcrypt.compareSync(body.password, usuarioDB.password)) {
+    if (!Usuario.password === body.password) {
+      return res.status(500).json({
+        ok: false,
+        mensaje: "Credenciales incorrectas - pass",
+        errors: err
+      });
+    }
+
+    //crear un token
+    var token = jwt.sign({ usuario:usuarioDB } , 'clave', { expiresIn: 14400 });
+
+    res.status(200).json({
+      ok:true,
+      usuario: usuarioDB,
+      token:token, 
+      id: usuarioDB._id
+    });
+
+
+  });
+
+}
+
+module.exports = {
+  login
+}
