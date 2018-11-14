f7893eac840aaaf4838719d746eab19568eb289a
marcosvillabasa <marcosvillabasa@gmail.com>
2018-11-14 04:30:51 -0300

    inicializamos el autoincremento, con la conexion a la bd y utilizamos el plugin

--- BackServer/Modelos/persona.js
+++ BackServer/Modelos/persona.js
@@ -4,17 +4,22 @@ let autoIncremento = require('mongoose-auto-increment');
 
 let Schema = mongoose.Schema;
 
+
+var connection = mongoose.createConnection("mongodb://adminBD:admin2018@ds155213.mlab.com:55213/proyecto-transporte-mm");
+
+autoIncremento.initialize(connection);
+
 let personaSchema = new Schema ({
   nombre: { type: String, required: [true, 'El nombre es necesario']},
   apellido: { type: String, required: [true, 'El apellido es necesario']},
   legajo: { type: Number, unique: true, required: [true, 'El legajo es necesario']},
   rol: {type: String, required: true},
-  img: {type: Strig, required: false},
+  img: {type: String, required: false},
   usuario: {type: Schema.Types.ObjectId, ref:'Usuario', required: [true, 'El usuario es requerido']},
 
 });
 
-personaSchema.plugin(personaSchema);
+personaSchema.plugin(uniqueValidator);
 personaSchema.plugin(autoIncremento.plugin, {model:'Persona', field: 'legajo'})
 
 module.exports = mongoose.model('Persona', personaSchema);
