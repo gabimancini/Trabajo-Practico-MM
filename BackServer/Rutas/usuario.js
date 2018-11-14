4fbd9bf52a4ed94dd70b71a48aceaffe1cf06f32
marcosvillabasa <marcosvillabasa@gmail.com>
2018-11-14 04:33:55 -0300

    implementamos el midleware en el post, put, delete

--- BackServer/Rutas/usuario.js
+++ BackServer/Rutas/usuario.js
@@ -6,15 +6,18 @@ const express = require('express');
 //importamos el controlador
 var UsuarioController = require('../Controladores/usuario.js');
 
+//importamos el middleware para usar en operaciones que necesiten validar el Token
+var md_auth = require('../middlewares/auth.js');
+
 //modulo Router de Express
 const app = express.Router();
 
 //Rutas de usuario
 
 app.get('/getUsuarios', UsuarioController.obtenerUsuarios);
-app.post('/registroUsuario', UsuarioController.crearUsuario);
-app.put('/:id', UsuarioController.actualizarUsuario);
-app.delete('/:id', UsuarioController.borrarUsuarioId);
+app.post('/registroUsuario', md_auth, UsuarioController.crearUsuario);
+app.put('/:id',md_auth, UsuarioController.actualizarUsuario);
+app.delete('/:id',md_auth, UsuarioController.borrarUsuarioId);
 
 
 //exportamos el Router
