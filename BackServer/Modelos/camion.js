6f83139c5f5807667a72d707ae737795c84efb01
marcosvillabasa <marcosvillabasa@gmail.com>
2018-11-20 22:37:55 -0300

    Modelo Camion modificado, la marca ahora es un atributo

--- BackServer/Modelos/camion.js
+++ BackServer/Modelos/camion.js
@@ -3,17 +3,12 @@ let uniqueValidator = require('mongoose-unique-validator');
 
 let Schema = mongoose.Schema;
 
-let marcaSchema = new Schema({
-  nombre: {type: String, required:true},
-  descripcion: {type: String}
-})
-
 let camionSchema = new Schema({
   patente: {type: String, unique:true, required: [true, 'La patente es necesaria']},
   capacidad: {type: Number, required:true},
   modelo: {type: String, required: false},
   legajo:{ type: Schema.Types.ObjectId, ref: 'Persona', required: [true, 'El camion tiene que tener un responsable']},
-  marca: [marcaSchema]
+  marca: {type:String, required: false}
 
 });
 
