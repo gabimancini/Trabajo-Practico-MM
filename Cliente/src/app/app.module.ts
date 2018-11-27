e98ae66f0ccb080f4db9f9dae252ce41a3ec51ab
marcosvillabasa <marcosvillabasa@gmail.com>
2018-11-27 12:12:57 -0300

    importamos moduos en el app module

--- Cliente/src/app/app.module.ts
+++ Cliente/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule }    from '@angular/common/http';
 
 import { AppComponent } from './app.component';
@@ -11,6 +11,17 @@ import { ContactoComponent } from './components/pages/contacto/contacto.componen
 import { HomeComponent } from './components/pages/home/home.component';
 import { LoginEmpleadorComponent } from './components/pages/login-empleador/login-empleador.component';
 import { CargodatosComponent } from './components/pages/cargodatos/cargodatos.component';
+import { LoginComponent } from './components/pages/login/login.component';
+import { DashboardComponent } from './components/pages/dashboard/dashboard.component';
+import { GraficasComponent } from './components/pages/graficas/graficas.component';
+import { ProgressComponent } from './components/pages/progress/progress.component';
+import { RegistroComponent } from './components/pages/login/registro.component';
+
+
+
+//modulos
+
+
 
 //Services
 import  { ServiciosEmpresaService  } from './services/servicios-empresa.service';
@@ -19,9 +30,7 @@ import { LoginEmpleadoComponent } from './components/pages/login-empleado/login-
 //Rutas
 import { RoutingModule } from './routing/routing.module';
 
- 
- 
-
+// import { ProgressComponent } from './components/pages/progress/progress.component';
 
 @NgModule({
   declarations: [
@@ -33,13 +42,23 @@ import { RoutingModule } from './routing/routing.module';
     HomeComponent,
     LoginEmpleadoComponent,
     LoginEmpleadorComponent,
-    CargodatosComponent
+    CargodatosComponent,
+    LoginComponent,
+    DashboardComponent,
+    GraficasComponent,
+    ProgressComponent,
+    RegistroComponent,
+
+
+
+
     ],
   imports: [
     BrowserModule,
     RoutingModule,
     FormsModule,
-    HttpClientModule
+    ReactiveFormsModule,
+    HttpClientModule,
   ],
   providers: [ServiciosEmpresaService ],
   bootstrap: [AppComponent]
