63aaf6f0ea3117ef1e0c48b685949813c9845069
marcosvillabasa <marcosvillabasa@gmail.com>
2018-11-27 11:35:17 -0300

    agregamos archivo config para el id y secret de google

---
+++ BackServer/Config/config.js
@@ -0,0 +1,5 @@
+module.exports.SEED = 'este-es-un-seed';
+
+//google
+module.exports.GOOGLE_CLIENT_ID = '262772475810-oi2lhp5lkhoi2dc4tdbrk28htllsbct5.apps.googleusercontent.com';
+module.exports.GOOGLE_SECRET = 'pTIg6EkkSCH987-C4nmxLLiK';
