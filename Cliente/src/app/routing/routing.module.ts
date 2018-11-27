ec08cafd23a8c63c9f0de0f739ab4c0061fb7390
marcosvillabasa <marcosvillabasa@gmail.com>
2018-11-27 12:15:16 -0300

    agregada la ruta de registro

--- Cliente/src/app/routing/routing.module.ts
+++ Cliente/src/app/routing/routing.module.ts
@@ -1,21 +1,34 @@
-import { NgModule } from '@angular/core';
+ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
 //componentes creados
-import { AppComponent } from '../app.component'; 
+import { AppComponent } from '../app.component';
 import { ContactoComponent } from '../components/pages/contacto/contacto.component';
 import { HomeComponent } from '../components/pages/home/home.component';
 import { LoginEmpleadoComponent } from '../components/pages/login-empleado/login-empleado.component';
 import { LoginEmpleadorComponent } from '../components/pages/login-empleador/login-empleador.component';
 import { CargodatosComponent } from '../components/pages/cargodatos/cargodatos.component';
+import { DashboardComponent } from '../components/pages/dashboard/dashboard.component';
+import { LoginComponent } from '../components/pages/login/login.component';
+import { ProgressComponent } from '../components/pages/progress/progress.component';
+import { GraficasComponent } from '../components/pages/graficas/graficas.component';
+import { RegistroComponent } from '../components/pages/login/registro.component';
 
 const routes: Routes = [
+
+  {path: 'home', component: HomeComponent},
+  {path: 'dashboard', component:DashboardComponent},
+  {path: 'progress', component:ProgressComponent},
+  {path: 'graficas', component:GraficasComponent},
 	{path: '', component: HomeComponent},
-	{path: 'home', component: HomeComponent},
 	{path: 'loginEmpleado', component: LoginEmpleadoComponent},
 	{path: 'loginEmpleador', component:LoginEmpleadorComponent},
-	{path: 'cargoDatos', component:CargodatosComponent}
+	{path: 'cargoDatos', component:CargodatosComponent},
+  {path: 'login', component:LoginComponent},
+  {path: 'registro', component:RegistroComponent},
+  {path: '', redirectTo: '/home', pathMatch:'full'},
+
 ];
 
 @NgModule({
