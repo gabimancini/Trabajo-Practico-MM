4ca366bcf91545a374ca83bb9ca2c2538267c9c3
marcosvillabasa <marcosvillabasa@gmail.com>
2018-11-27 12:09:36 -0300

    ejemplo de login con local storage

--- Cliente/src/app/app.component.ts
+++ Cliente/src/app/app.component.ts
@@ -7,4 +7,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'logistic';
+
+  // constructor(){
+  //   localStorage.removeItem("logueado");
+  //   if(this.usuarioService.chequearSiUserEstaLogueado){
+  //     this.router.navigate("/escritorio")
+  //   }
+  //   /*
+  //   let datosUsuario = {
+  //     id:"1232313",
+  //     nombre: "JUan"
+  //   };
+  //   Convierto de JSON a CADENA
+  //   let logueado = localStorage.setItem("logueado", JSON.stringify(datosUsuario));
+  //   CONVIERTO DE CADENA A JSON
+  //   let datosParseados = JSON.parse(localStorage.getItem("logueado"));
+  //
+  //   if(logueado == "true"){
+  //     console.log("Estas logueado")
+  //   }else{
+  //     console.log("No estas logueado")
+  //   }
+  //   */
+  // }
 }
