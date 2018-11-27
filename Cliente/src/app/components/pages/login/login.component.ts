806cbe5f4b1ce38dbe4097fe50eda63ecd7cac7c
marcosvillabasa <marcosvillabasa@gmail.com>
2018-11-27 14:45:51 -0300

    creado el metodo ingresar(forma)

--- Cliente/src/app/components/pages/login/login.component.ts
+++ Cliente/src/app/components/pages/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '../../../../../node_modules/@angular/forms';
+import { UsuarioService } from '../../../services/service.index';
+import { Router } from '../../../../../node_modules/@angular/router';
+import { Usuario } from '../../../modelos/usuario.model';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public _usuarioService: UsuarioService,
+    public router:Router
+  ) { }
 
   ngOnInit() {
+  }
+
+  ingresar(forma: NgForm){
+
+    if(forma.invalid){
+      return;
+    }
+
+    let usuario = new Usuario (forma.value.email, forma.value.clave);
+
+    this._usuarioService.login(usuario)
+        .subscribe(resp => {
+          console.log(resp);
+        });
   }
 
 }
