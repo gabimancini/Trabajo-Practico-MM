d76b445b877e6c87a1844ae2142a475bcc205bf4
marcosvillabasa <marcosvillabasa@gmail.com>
2018-11-27 11:32:48 -0300

    implementamos el middleware CORS

--- BackServer/index.js
+++ BackServer/index.js
@@ -8,6 +8,15 @@ var bodyParser = require('body-parser');
 // Iniciar express
 const app = express();
 
+
+//middle cors
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
+  next();
+});
+
 //body-parser  x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
